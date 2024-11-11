7f6907bcb0f8bf2931e1c771cb614aa9e03ef047
Patrick Mortara <mortara@gmail.com>
2024-11-11 09:46:44 +0100

    Added direct conversion toMagickImage

--- src/FromSKImage/ToMagickNet.cs
+++ src/FromSKImage/ToMagickNet.cs
@@ -10,6 +10,48 @@ namespace PMortara.Helpers.ImageConverterExtensions
     public static partial class SKImageExtensions
     {
         /// <summary>
+        /// Converts SKBitmap to IMagickImage
+        /// </summary>
+        /// <param name="skimg"></param>
+        /// <returns></returns>
+        /// <ToDo>
+        /// Improve handling of different pixelformats
+        /// </ToDo>
+        public static IMagickImage ToMagickImage(this SKImage skimg)
+        {
+            if(skimg.IsLazyGenerated)
+            {
+                var bmp = SKBitmap.FromImage(skimg);
+
+                return bmp.ToMagickImage();
+            }
+
+
+            var pixels = skimg.PeekPixels().GetPixelSpan();
+            
+            var settings = new MagickReadSettings();
+            settings.Width = (uint)skimg.Width;
+            settings.Height = (uint)skimg.Height;
+
+            switch (skimg.ColorType)
+            {
+                case SKColorType.Rgba8888:
+                    settings.Depth = 8;
+                    settings.Format = MagickFormat.Rgba;
+                    break;
+                case SKColorType.Bgra8888:
+                    settings.Depth = 8;
+                    settings.Format = MagickFormat.Bgra;
+                    break;
+            }
+
+            
+            
+            return new MagickImage(pixels, settings);
+
+        }
+
+        /// <summary>
         /// Converts SKImage to IMagickImage
         /// </summary>
         /// <param name="skimg"></param>
@@ -17,7 +59,7 @@ namespace PMortara.Helpers.ImageConverterExtensions
         /// <ToDo>
         /// Get rid of that FromImage() step.
         /// </ToDo>
-        public static IMagickImage ToMagickImage(this SKImage skimg)
+        public static IMagickImage ToMagickImage_v1(this SKImage skimg)
         {
             var bmp = SKBitmap.FromImage(skimg);
             
