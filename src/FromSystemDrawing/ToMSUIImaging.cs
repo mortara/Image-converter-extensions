fc34fa9fd8a08728da6c343cebd1b4b41155ed5b
Patrick Mortara <mortara@gmail.com>
2024-11-11 09:46:07 +0100

    Cleaned up usings

--- src/FromSystemDrawing/ToMSUIImaging.cs
+++ src/FromSystemDrawing/ToMSUIImaging.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.UI.Xaml.Media.Imaging;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Windows.Media;
 
 namespace PMortara.Helpers.ImageConverterExtensions
 {
     public static partial class SystemDrawingExtensions
     {
         /// <summary>
-        /// Converts a System.Drawing.Bitmap to a Microsoft.UI.Xaml.Media.Imaging.BitmapImage        /// </summary>
+        /// Converts a System.Drawing.Bitmap to a Microsoft.UI.Xaml.Media.Imaging.BitmapImage        
+        /// </summary>
         /// <param name="bitmap"></param>
         /// <returns></returns>
         public static BitmapImage ToBitmapImage(this Bitmap bitmap)
@@ -31,7 +31,7 @@ namespace PMortara.Helpers.ImageConverterExtensions
         public static WriteableBitmap ToWriteableBitmap(this Bitmap bitmap)
         {
             var bitmapImage = new WriteableBitmap(bitmap.Width, bitmap.Height);
-
+            
             var format = ImageFormat.Bmp;
 
             using (var stream = new MemoryStream())
