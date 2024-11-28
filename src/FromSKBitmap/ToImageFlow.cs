a52f46ea848d7a5884fbe8005b17a69747dd02c2
Patrick Mortara <mortara@gmail.com>
2024-11-28 16:06:23 +0100

    Added ImageFlow conversions

---
+++ src/FromSKBitmap/ToImageFlow.cs
@@ -0,0 +1,28 @@
+﻿using Imageflow.Bindings;
+using Imageflow.Fluent;
+using SkiaSharp;
+using System.Drawing;
+
+namespace PMortara.Helpers.ImageConverterExtensions.FromSKBitmap
+{
+    public static partial class SKBitmapExtensions
+    {
+     
+        public static BuildNode ToImageFlowBuildNode(this SKBitmap skbmp)
+        {
+
+            using (var pixmap = skbmp.PeekPixels())
+            {
+                var filters = SKPngEncoderFilterFlags.NoFilters;
+                int compress = 0;
+                var options = new SKPngEncoderOptions(filters, compress);
+                using (var data = pixmap.Encode(options))
+                {
+                    var job = new ImageJob();
+                    return job.Decode(data.ToArray());
+                }
+            }
+
+        }
+    }
+}
