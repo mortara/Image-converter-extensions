fd8fb35010f74aee7d46615744ddec6dbe2bd0e3
Patrick Mortara <mortara@gmail.com>
2024-12-28 20:11:18 +0100

    Improved the conversion

--- src/FromSKBitmap/ToImageSharp.cs
+++ src/FromSKBitmap/ToImageSharp.cs
@@ -1,4 +1,5 @@
 ﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using SkiaSharp;
 using System.IO;
 using Image = SixLabors.ImageSharp.Image;
@@ -6,25 +7,37 @@ using Image = SixLabors.ImageSharp.Image;
 namespace PMortara.Helpers.ImageConverterExtensions.FromSKBitmap
 {
     public static partial class SKBitmapExtensions
-    {
+    {     
         /// <summary>
         /// Converts a SKBitmap into a ImageSharp.Image. 
         /// </summary>
-        /// <param name="mimg"></param>
+        /// <param name="skiaBitmap"></param>
         /// <returns></returns>
-        /// <ToDo>
-        /// Get rid of PNG re-encoding
-        /// </ToDo>
         public static Image ToImageSharpImage(this SKBitmap skiaBitmap)
         {
-            using (var ms = new MemoryStream())
+            /// Directy load Pixeldata for known ColorTypes
+            if (skiaBitmap.ColorType == SKColorType.Rgba8888)
+                return Image.LoadPixelData<Rgba32>(skiaBitmap.GetPixelSpan(), skiaBitmap.Width, skiaBitmap.Height);
+
+            if (skiaBitmap.ColorType == SKColorType.Bgra8888)
+                return Image.LoadPixelData<Bgra32>(skiaBitmap.GetPixelSpan(), skiaBitmap.Width, skiaBitmap.Height);
+
+            if (skiaBitmap.ColorType == SKColorType.Gray8)
+                return Image.LoadPixelData<L8>(skiaBitmap.GetPixelSpan(), skiaBitmap.Width, skiaBitmap.Height);
+
+
+            // All other Colortypes are processed pixel by pixel
+            var image = new Image<Rgba32>(skiaBitmap.Width, skiaBitmap.Height);
+            for (int y = 0; y < skiaBitmap.Height; y++)
             {
-                skiaBitmap.Encode(ms, SKEncodedImageFormat.Png, 100);
-                ms.Position = 0;
-
-                return Image.Load(ms);
-
+                for (int x = 0; x < skiaBitmap.Width; x++)
+                {
+                    var color = skiaBitmap.GetPixel(x, y);
+                    image[x, y] = new Rgba32(color.Red, color.Green, color.Blue, color.Alpha);
+                }
             }
+
+            return image;
         }
     }
 }
