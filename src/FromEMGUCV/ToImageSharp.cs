f8eba6116efee437f6bc6c1a4a86f1a2c3b22c1b
Patrick Mortara <mortara@gmail.com>
2024-12-29 01:12:06 +0100

    Improved AsImageSharpImage method

--- src/FromEMGUCV/ToImageSharp.cs
+++ src/FromEMGUCV/ToImageSharp.cs
@@ -1,4 +1,8 @@
 ﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using SixLabors.ImageSharp.PixelFormats;
+using SkiaSharp;
+using System;
 using Image = SixLabors.ImageSharp.Image;
 
 namespace PMortara.Helpers.ImageConverterExtensions
@@ -18,6 +22,48 @@ namespace PMortara.Helpers.ImageConverterExtensions
             var jpegbytes = image.ToJpegData();
 
             return Image.Load(jpegbytes);
+        }
+
+        /// <summary>
+        /// Wraps a EMGU.CV image into a ImageSharp Image without copying the pixel-data
+        /// </summary>
+        /// <typeparam name="TColor"></typeparam>
+        /// <typeparam name="TDepth"></typeparam>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static Image AsImageSharpImage<TColor, TDepth>(this Image<TColor, TDepth> image) where TColor : struct, IColor where TDepth : new()
+        {
+            Type typeFromHandle = typeof(TColor);
+            Type depthFromHandle = typeof(TDepth);
+            unsafe
+            {
+                var l = image.NumberOfChannels * image.Width * image.Height;
+                var memory = image.Mat.DataPointer.ToPointer();
+
+                if (typeFromHandle == typeof(Gray))
+                {
+                    return Image.WrapMemory<L8>(memory, l, image.Width, image.Height);
+                }
+                else if (typeFromHandle == typeof(Bgra))
+                {
+                    return Image.WrapMemory<Bgra32>(memory, l, image.Width, image.Height);
+                }
+                else if (typeFromHandle == typeof(Rgba))
+                {
+                    return Image.WrapMemory<Rgba32>(memory, l, image.Width, image.Height);
+                }
+                else if (typeFromHandle == typeof(Bgr))
+                {
+                    return Image.WrapMemory<Bgr24>(memory, l, image.Width, image.Height);
+                }
+                else if (typeFromHandle == typeof(Rgb))
+                {
+                    return Image.WrapMemory<Rgb24>(memory, l, image.Width, image.Height);
+                }
+            }
+
+            throw new Exception("ColorType not supported!");
         }
     }
 }
